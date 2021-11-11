144d596bd2f6251003b1c60955af44938aa1634a
AbrarChowdhry98 <abrarchowdhry44@gmail.com>
2021-11-11 13:38:28 +0500

    Debugged mysql

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,12 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: 'localhost',
+      user:'root',
+      password:'2197',
       dialect: 'mysql',
+      database: 'ecommerce_db',
       dialectOptions: {
         decimalNumbers: true,
       },
